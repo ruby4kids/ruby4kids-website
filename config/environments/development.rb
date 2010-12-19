be717f83170fd19e3e69535f77be4b5ffc9771b6
Anthony Burns <anthony@dirtyalpaca.com>
2010-12-19 02:11:42 -0500

    Set default_url_options for ActionMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Ruby4Kids::Application.configure do
   config.whiny_nils                             = true
   config.action_controller.perform_caching      = false
   config.action_dispatch.best_standards_support = :builtin
+  config.action_mailer.default_url_options      = { host: 'localhost:3000' }
   config.action_mailer.raise_delivery_errors    = false
   config.active_support.deprecation             = :log
   config.action_view.debug_rjs                  = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,6 +5,7 @@ Ruby4Kids::Application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_controller.perform_caching          = false
   config.action_dispatch.show_exceptions            = false
+  config.action_mailer.default_url_options          = { host: 'ruby4kids.test' }
   config.action_mailer.delivery_method              = :test
   config.active_support.deprecation                 = :stderr
 end
